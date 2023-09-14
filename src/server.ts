6edf59768e6124b44d3fcc44154ea49006106eb4
lucjastozek <lucjastozek@gmail.com>
2023-09-14 22:26:52 +0200

    Initial commit

--- src/server.ts
+++ src/server.ts
@@ -1,48 +1,41 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-import { Client } from "pg";
-import { getEnvVarOrFail } from "./support/envVarUtils";
-import { setupDBClientConfig } from "./support/setupDBClientConfig";
+import * as cheerio from "cheerio";
+import fetch from "node-fetch";
 
-dotenv.config(); //Read .env file lines as though they were env vars.
+dotenv.config();
 
-const dbClientConfig = setupDBClientConfig();
-const client = new Client(dbClientConfig);
-
-//Configure express routes
 const app = express();
 
-app.use(express.json()); //add JSON body parser to each following route handler
-app.use(cors()); //add CORS support to each following route handler
+app.use(express.json());
+app.use(cors());
 
 app.get("/", async (_req, res) => {
-    res.json({ msg: "Hello! There's nothing interesting for GET /" });
+    res.json({ msg: "Hello! There's nothing interesting for GET /." });
 });
 
-app.get("/health-check", async (_req, res) => {
+app.get("/image", async (req, res) => {
+    const { link } = req.body;
+
     try {
-        //For this to be successful, must connect to db
-        await client.query("select now()");
-        res.status(200).send("system ok");
+        const response = await fetch(link);
+        const html = await response.text();
+        const $ = cheerio.load(html);
+
+        const imageUrl =
+            $('meta[property="og:image"]').attr("content") ||
+            $("img").attr("src");
+
+        res.send(imageUrl);
     } catch (error) {
-        //Recover from error rather than letting system halt
         console.error(error);
         res.status(500).send("An error occurred. Check server logs.");
     }
 });
 
-connectToDBAndStartListening();
-
-async function connectToDBAndStartListening() {
-    console.log("Attempting to connect to db");
-    await client.connect();
-    console.log("Connected to db!");
-
-    const port = getEnvVarOrFail("PORT");
-    app.listen(port, () => {
-        console.log(
-            `Server started listening for HTTP requests on port ${port}.  Let's go!`
-        );
-    });
-}
+app.listen(4000, () => {
+    console.log(
+        `Server started listening for HTTP requests on port ${4000}.  Let's go!`
+    );
+});
